6b72c7f1eabf2dcca02d736de2b2fc1f60f8fa85
Mounir <mounir@mobilabsolutions.com>
2023-07-07 14:57:26 +0200

    asdad

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,8 +1,2 @@
-variable "location" {
-  description = "(Required) The location of the Resource Group."
-}
-variable "location_abbreviation" {
-  description = "(Required) The abbreviation of the location used in the Resource Group name."
-}
 location = "West Europe"
 location_abbreviation = "weu"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "location" {
+  description = "(Required) The location of the Resource Group."
+}
+
+variable "location_abbreviation" {
+  description = "(Required) The abbreviation of the location used in the Resource Group name."
+}
+
 variable "sku_size" {
   type        = string
   description = "value"
